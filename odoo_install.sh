0a6455da4cd3c9c0c992014f90540557ac49f27b
Bassam Mannaa <31972355+bassammannaa@users.noreply.github.com>
2019-01-22 15:41:29 +0300

    Update odoo_install.sh

--- odoo_install.sh
+++ odoo_install.sh
@@ -21,7 +21,7 @@ OE_HOME_EXT="/$OE_USER/${OE_USER}-server"
 # Set to true if you want to install it, false if you don't need it or have it already installed.
 INSTALL_WKHTMLTOPDF="True"
 # Set the default Odoo port (you still have to use -c /etc/odoo-server.conf for example to use this.)
-OE_PORT="8069"
+OE_PORT="80"
 # Choose the Odoo version which you want to install. For example: 12.0, 11.0, 10.0 or saas-18. When using 'master' the master version will be installed.
 # IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 12.0
 OE_VERSION="12.0"
